a4693b9c5322235b972da2e8714df2dac550819f
Mohammad Javad <javadghodosy81@gmail.com>
2025-01-23 09:11:45 +0330

    remove junk code

--- freetypeLib.jl
+++ freetypeLib.jl
@@ -2,17 +2,13 @@
 module ftmod
 
 using FreeType
-# FT_Error : Int32
-# FT_UInt : UInt32
-# FT_Face : Ptr{Nothing}
+
 function ftInit(fontfilepath, faceindex = Int32(0))
 
-    library = Ref{Ptr{Nothing}}() # fill(Ptr{Nothing}(), 1)
+    library = Ref{Ptr{Nothing}}()
     @ccall (:libfreetype).FT_Init_FreeType(library::Ptr{Nothing})::Cint
 
     face = Ref{Ptr{FT_FaceRec_}}()
-    # Ref{Ptr{Nothing}}()
-    # Ref{FT_Face}()
 
     @ccall (:libfreetype).FT_New_Face(
             library[]::Ptr{Nothing}, fontfilepath::Ptr{UInt8},
@@ -83,64 +79,3 @@ export ftInit, ftDoneFace, ftDoneLibrary, setPixelSize!,
 end
 
 using .ftmod
-
-# fHeight = floor(Int, (flod.ascender-flod.descender)* yScale// 64)
-# btop = floor(Int, (vAdvance-(flod.descender//64))*yScale) # - gb.rows
-# btop = floor(Int, glyph.metrics.vertBearingY/64)
-# floor(Int, glyph.metrics.horiBearingY/64)
-
-# hAdvance = floor(Int, glyph.metrics.horiAdvance/64)
-# vAdvance = floor(Int, glyph.metrics.vertAdvance/64)
-
-# mutable struct rasterParams
-#     target::Ptr{FT_Bitmap}
-#     source::Ptr{Cvoid}
-#     flags::Cint
-#     gray_spans::FT_SpanFunc
-#     black_spans::FT_SpanFunc
-#     bit_test::FT_Raster_BitTest_Func
-#     bit_set::FT_Raster_BitSet_Func
-#     user::Ptr{Cvoid}
-#     clip_box::FT_BBox
-
-# end
-
-# bbo = Ref{FT_BBox}()
-# bmap = Ref{FT_Bitmap}()
-# params = Ref(rasterParams())
-# params[].target = pointer_from_objref( bmap )
-# params[].flags = 1
-# FT_Outline_Render(library, Ref{FT_Outline}(glyph.outline), Ptr{FT_Raster_Params}(pointer_from_objref(params)) ) 
-
-
-# struct FT_BBox
-#     xMin::Int64
-#     yMin::Int64
-#     xMax::Int64
-#     yMax::Int64
-# end
-# bbox::FT_BBox
-# units_per_EM::UInt16
-# ascender::Int16
-# descender::Int16
-# height::Int16
-# max_advance_width::Int16
-# max_advance_height::Int16
-# FT_UShort: UInt16
-# FT_Short: Int16
-
-
-# sizeRequestType::UInt32 => 0: NOMINAL | 1: REAL_DIM | 2: BBOX | 3: CELL | 4: SCALES
-# struct sizeRequestRec
-#     type::UInt32
-#     width::Int64
-#     height::Int64
-#     horiResolution::UInt32
-#     vertResolution::UInt32
-# end
-
-# requestSize(face, req) = 
-#             ccall((:FT_Request_Size, :libfreetype), Int32, 
-#                             (Ptr{Nothing}, Ptr{sizeRequestRec}), face, req)
-# reqInst = Ref(sizeRequestRec(0, 40, 50, 70, 80))
-# requestSize(face, reqInst)
